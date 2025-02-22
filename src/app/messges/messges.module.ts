5e8c18a8166a15c2cafd3e863f3e015e2aaadca3
jidni911 <gidni441@gmail.com>
2025-02-22 22:26:19 +0600

    22 feb

--- src/app/messges/messges.module.ts
+++ src/app/messges/messges.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { MessgesRoutingModule } from './messges-routing.module';
 import { MessgesComponent } from './messges/messges.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { MessgesComponent } from './messges/messges.component';
   ],
   imports: [
     CommonModule,
-    MessgesRoutingModule
+    MessgesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class MessgesModule { }
