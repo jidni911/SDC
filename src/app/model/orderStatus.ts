d237d0fe2e1936458be8118ec0e608619d9ba624
jidni911 <gidni441@gmail.com>
2025-02-22 23:30:52 +0600

    Auto-generated commit

--- src/app/model/orderStatus.ts
+++
@@ -1,9 +0,0 @@
-export enum OrderStatus {
-    PENDING = 'PENDING', // just placed
-    REJECTED = 'REJECTED', // Rejected
-    PROCESSING = 'PROCESSING', // Accepted
-    OUT_FOR_DELIVERY = 'OUT_FOR_DELIVERY',
-    COMPLETED = 'COMPLETED', // delivered
-    CANCELLED = 'CANCELLED',
-    REFUSED = 'REFUSED'
-}
--- src/app/replace-space.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReplaceSpacePipe } from './replace-space.pipe';
-
-describe('ReplaceSpacePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ReplaceSpacePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
