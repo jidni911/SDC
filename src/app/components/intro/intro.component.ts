4e601c9e5f7b4bd63aab167038699d532e33a2c8
jidni911 <gidni441@gmail.com>
2025-04-14 12:33:51 +0600

    added intro images

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -9,5 +9,7 @@ export class IntroComponent {
  bg1 = 'assets/photo/image1.png';
  sdcbijoyjersey25 = 'assets/photo/sdcbijoyjersey25.png';
  sdcbijoyjersey21 = 'assets/photo/sdcbijoyjersey21.png';
- sdcofficialjersey23 = 'assets/photo/sdcofficialjersey23.png'; 
+ sdcofficialjersey23 = 'assets/photo/sdcofficialjersey23.png';
+ savegazaevent = 'assets/photo/savegazaevent.png';
+ barishalride = 'assets/photo/barishalride.png'; 
 }
