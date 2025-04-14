ceaa0064c54281df2ebe4d431429341df0270e73
jidni911 <gidni441@gmail.com>
2025-04-14 12:27:51 +0600

    added into images

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class IntroComponent {
  bg1 = 'assets/photo/image1.png';
+ sdcbijoyjersey25 = 'assets/photo/sdcbijoyjersey25.png';
+ sdcbijoyjersey21 = 'assets/photo/sdcbijoyjersey21.png';
+ sdcofficialjersey23 = 'assets/photo/sdcofficialjersey23.png'; 
 }
