a4c85419e15cac59ec1ec86ac47097a5ed3a0557
jidni911 <gidni441@gmail.com>
2025-04-25 11:14:48 +0600

    updated backend

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://ideally-precious-fish.ngrok-free.app',
+    // apiUrl: 'https://ideally-precious-fish.ngrok-free.app',
+    apiUrl: 'https://sdcian.serveo.net',
     // apiUrl: 'http://localhost:3000',
+    // apiUrl: 'http://localhost:8081',
     // apiUrl: 'https://sdcianspring-production-17a5.up.railway.app',
     basePath : '/'
 };
